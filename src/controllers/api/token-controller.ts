0bcf2164ac9f6e535a2c7b8dcd25528ac71c880d
David Heinebäck <du222aa@student.lnu.se>
2022-02-09 15:36:54 +0100

    refactor: implement register and login controllers with jwt

--- src/controllers/api/token-controller.ts
+++ src/controllers/api/token-controller.ts
@@ -1,107 +1,18 @@
 import createError from 'http-errors'
-import { UserModel } from '../../models/UserModel'
-import * as globalMethod from './method-controller'
+import { Request, Response, NextFunction } from 'express'
+import { getAccessAndRefreshToken } from './method-controller'
+import { addUser, authorizeUser } from '../../repository/user-repository'
+import { TokenInterface } from '../../models/TokenModel'
+import { setToken } from '../../repository/token-repository'
 
 /**
  * Encapsulates a controller.
  */
 export class TokenController {
-  /**
-   * Authenticates a user.
-   *
-   * @param {object} req - Express request object.
-   * @param {object} res - Express response object.
-   * @param {Function} next - Express next middleware function.
-   */
-  async login (req, res, next) {
-    try {
-      const user = await UserModel.authenticate(req.body.username, req.body.password)
-      const payload = {
-        sub: user.username,
-        isAdmin: user.isAdmin
-      }
 
-      // Create the access token with the shorter lifespan.
-      const accessToken = globalMethod.getAccessToken(payload)
-
-      // Create the access token with the shorter lifespan.
-      const refreshToken = globalMethod.getRefreshToken(payload)
-
-      await UserModel.setToken(user.username, refreshToken)
-      res
-        .status(200)
-        .json({
-          user: payload,
-          access_token: accessToken,
-          refresh_token: refreshToken
-        })
-    } catch (error) {
-      // Authentication failed.
-      const err = createError(401)
-      err.innerException = error
-
-      next(err)
-    }
-  }
-
-  /**
-   * Logout a user.
-   *
-   * @param {object} req - Express request object.
-   * @param {object} res - Express response object.
-   * @param {Function} next - Express next middleware function.
-   * @returns {status} - Returns a status representing the logout proccess
-   */
-  async logout (req, res, next) {
-    try {
-      await UserModel.logout(req.body.user)
-      res.sendStatus(204)
-    } catch (error) {
-      next(error)
-    }
-  }
-
-  /**
-   * Authorize the user by validating the token.
-   *
-   * @param {object} req - Express request object.
-   * @param {object} res - Express response object.
-   * @param {Function} next - Express next middleware function.
-   * @returns {Error} - Returns a error if user validation is failed.
-   */
-  async authenticateToken (req, res, next) {
-    try {
-      const userobj = await JSON.parse(Buffer.from(req.headers.authorization.split(' ')[1]).toString('ascii'))
-      if (userobj.access_token === null) return res.sendStatus(401)
-      const authObject = { body: userobj, secret: process.env.ACCESS_TOKEN_SECRET }
-      const user = await User.findOne({ username: userobj?.user?.sub })
-      const token = globalMethod.accessCheckCall(authObject, user)
-      if (token) {
-        res.token = token
-        next()
-      } else {
-        res.sendStatus(401)
-      }
-    } catch (error) {
-      res.status(403)
-    }
-  }
-
-  /**
-   * Sends the response.
-   *
-   * @param {object} req - Express request object.
-   * @param {object} res - Express response object.
-   * @returns {Error} - Returns a error if user validation is failed.
-   */
-  async responseHandler (req, res) {
-    const token = res.token
-    if (token) {
-      res.status(200).json(token)
-    } else {
-      res.sendStatus(401)
-    }
-  }
+index () {
+  console.log('Hello!')
+}
 
   /**
    * Registers a user.
@@ -110,17 +21,19 @@ export class TokenController {
    * @param {object} res - Express response object.
    * @param {Function} next - Express next middleware function.
    */
-  async register (req, res, next) {
+  async register (req: Request, res: Response, next: NextFunction) {
     try {
-      const user = await UserModel.insert({
-        username: req.body.username,
+      const user = await addUser({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
         password: req.body.password
       })
 
       res
         .status(201)
-        .json({ id: user.id })
-    } catch (error) {
+        .json({ id: user.email })
+    } catch (error: any) {
       let err = error
 
       if (err.code === 11000) {
@@ -135,4 +48,103 @@ export class TokenController {
       next(err)
     }
   }
+
+
+  /**
+   * Authenticates a user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async login (req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await authorizeUser(req.body.email, req.body.password)
+      const payload = {
+        sub: user.email
+      }
+
+      const tokens = getAccessAndRefreshToken(payload)
+
+      const refreshToken: TokenInterface = {
+        user: user.email,
+        refreshToken: tokens.refresh_token
+      }
+
+      await setToken(refreshToken)
+      
+      res
+        .status(200)
+        .json({
+          user: payload,
+          access_token: tokens.access_token,
+          refresh_token: tokens.refresh_token
+        })
+    } catch (error) {
+      // Authentication failed.
+      const err = createError(401)
+      err.innerException = error
+
+      next(err)
+    }
+  }
+
+  // /**
+  //  * Logout a user.
+  //  *
+  //  * @param {object} req - Express request object.
+  //  * @param {object} res - Express response object.
+  //  * @param {Function} next - Express next middleware function.
+  //  * @returns {status} - Returns a status representing the logout proccess
+  //  */
+  // async logout (req, res, next) {
+  //   try {
+  //     await UserModel.logout(req.body.user)
+  //     res.sendStatus(204)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+
+  // /**
+  //  * Authorize the user by validating the token.
+  //  *
+  //  * @param {object} req - Express request object.
+  //  * @param {object} res - Express response object.
+  //  * @param {Function} next - Express next middleware function.
+  //  * @returns {Error} - Returns a error if user validation is failed.
+  //  */
+  // async authenticateToken (req, res, next) {
+  //   try {
+  //     const userobj = await JSON.parse(Buffer.from(req.headers.authorization.split(' ')[1]).toString('ascii'))
+  //     if (userobj.access_token === null) return res.sendStatus(401)
+  //     const authObject = { body: userobj, secret: process.env.ACCESS_TOKEN_SECRET }
+  //     const user = await User.findOne({ username: userobj?.user?.sub })
+  //     const token = globalMethod.accessCheckCall(authObject, user)
+  //     if (token) {
+  //       res.token = token
+  //       next()
+  //     } else {
+  //       res.sendStatus(401)
+  //     }
+  //   } catch (error) {
+  //     res.status(403)
+  //   }
+  // }
+
+//   /**
+//    * Sends the response.
+//    *
+//    * @param {object} req - Express request object.
+//    * @param {object} res - Express response object.
+//    * @returns {Error} - Returns a error if user validation is failed.
+//    */
+//   async responseHandler (req, res) {
+//     const token = res.token
+//     if (token) {
+//       res.status(200).json(token)
+//     } else {
+//       res.sendStatus(401)
+//     }
+//   }
 }
