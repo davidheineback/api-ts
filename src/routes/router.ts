956b790f36fd9ffdace1c16dae43286ea836eeb5
David Heinebäck <du222aa@student.lnu.se>
2022-02-10 13:50:50 +0100

    remove: v1 from URI

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,6 +4,6 @@ import { router as v1Router } from './api/v1/router'
 
 export const router = express.Router()
 
-router.use('/api/v1', v1Router)
+router.use('/api', v1Router)
 
 router.use('*', (req: Request, res: Response, next: NextFunction) => next(createError(404)))
