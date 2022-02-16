029699cc31aa542390de5c11a4580912ad8b4887
David Heinebäck <du222aa@student.lnu.se>
2022-02-16 15:00:17 +0100

    refactor: implement getAccessToken on refresh and add to response

--- src/controllers/api/token-controller.ts
+++ src/controllers/api/token-controller.ts
@@ -1,6 +1,6 @@
 import createError from 'http-errors'
 import { Request, Response, NextFunction } from 'express'
-import { getAccessAndRefreshToken, verifyRefreshToken, verifyAccessToken } from './method-controller'
+import { getAccessAndRefreshToken, verifyRefreshToken, verifyAccessToken, getAccessToken } from './method-controller'
 import { addUser, authorizeUser } from '../../repository/user-repository'
 import { TokenInterface } from '../../models/TokenModel'
 import { setToken, deleteToken, getTokenByEmail, getRefreshToken } from '../../repository/token-repository'
@@ -112,11 +112,20 @@ console.log(paths)
   async refresh(req: Request, res: Response, next: NextFunction) {
     const [Bearer, token] = <string[]>req.headers.authorization?.split(' ')
     if (Bearer === 'Bearer') {
+    
       try {
         const refreshToken = await getRefreshToken(token)
         if (refreshToken) {
+          console.log('LOOOL')
           verifyRefreshToken(token)
-          res.sendStatus(204)
+          const payload = { sub: 'username' }
+          const newAccessToken = getAccessToken(payload)
+          res
+          .status(200)
+          .json({
+            user: payload,
+            access_token: newAccessToken
+          })
         } else {
           next(createError(401))
         }
@@ -135,11 +144,12 @@ console.log(paths)
   }
 
   access(req: Request, res: Response, next: NextFunction) {
+    console.log('acccesss')
     const [Bearer, token] = <string[]>req.headers.authorization?.split(' ')
     if (Bearer === 'Bearer') {
       try {
         verifyAccessToken(token)
-        res.sendStatus(204)
+        next()
       } catch (error) {
         const err = createError(401)
         err.innerException = error
@@ -148,45 +158,4 @@ console.log(paths)
     }
   }
 
-  // /**
-  //  * Authorize the user by validating the token.
-  //  *
-  //  * @param {object} req - Express request object.
-  //  * @param {object} res - Express response object.
-  //  * @param {Function} next - Express next middleware function.
-  //  * @returns {Error} - Returns a error if user validation is failed.
-  //  */
-  // async authenticateToken (req, res, next) {
-  //   try {
-  //     const userobj = await JSON.parse(Buffer.from(req.headers.authorization.split(' ')[1]).toString('ascii'))
-  //     if (userobj.access_token === null) return res.sendStatus(401)
-  //     const authObject = { body: userobj, secret: process.env.ACCESS_TOKEN_SECRET }
-  //     const user = await User.findOne({ username: userobj?.user?.sub })
-  //     const token = globalMethod.accessCheckCall(authObject, user)
-  //     if (token) {
-  //       res.token = token
-  //       next()
-  //     } else {
-  //       res.sendStatus(401)
-  //     }
-  //   } catch (error) {
-  //     res.status(403)
-  //   }
-  // }
-
-//   /**
-//    * Sends the response.
-//    *
-//    * @param {object} req - Express request object.
-//    * @param {object} res - Express response object.
-//    * @returns {Error} - Returns a error if user validation is failed.
-//    */
-//   async responseHandler (req, res) {
-//     const token = res.token
-//     if (token) {
-//       res.status(200).json(token)
-//     } else {
-//       res.sendStatus(401)
-//     }
-//   }
 }
