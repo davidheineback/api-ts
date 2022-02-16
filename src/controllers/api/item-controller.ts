73de61e5ca54e92021c0b71e84e9c6d9616ee866
David Heinebäck <du222aa@student.lnu.se>
2022-02-16 15:51:59 +0100

    add: item-controller

---
+++ src/controllers/api/item-controller.ts
@@ -0,0 +1,26 @@
+import createError from 'http-errors'
+import { Request, Response, NextFunction } from 'express'
+import { getAssociatedLinks, Links } from '../../helpers/hateoas'
+
+/**
+ * Encapsulates a controller.
+ */
+export class ItemController {
+
+index (req: Request, res: Response, next: NextFunction) {
+  console.log(req.params)
+  console.log('Hello from Auction index')
+  const self =  `${req.protocol}://${req.get('host')}${req.originalUrl}`
+    const linkSelection: Links = {
+      register: true,
+      login: true,
+    }
+
+const paths = getAssociatedLinks(self, linkSelection)
+// console.log(paths)
+
+  res.json({ message: 'Authentication operations:', links: paths })
+}
+
+
+}
