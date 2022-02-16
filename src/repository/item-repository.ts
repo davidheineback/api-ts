1ce2c603d33b5975b848dc07da76bdb1a708a8cb
David Heinebäck <du222aa@student.lnu.se>
2022-02-16 15:43:06 +0100

    add: Item to repository layer

---
+++ src/repository/item-repository.ts
@@ -0,0 +1,7 @@
+import { ItemInterface, ItemModel } from '../models/ItemModel'
+
+export async function addItem(item: ItemInterface): Promise<ItemInterface> {
+  const newItem= new ItemModel(item)
+  const saveItem= await newItem.save()
+  return saveItem
+}
